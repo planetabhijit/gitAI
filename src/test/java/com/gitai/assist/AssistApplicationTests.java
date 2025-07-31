1f37d281b2ead4240d839c7f188dcda41a8e7a14
abhijit <abhijitpatelraju@gmail.com>
2025-07-31 10:35:41 +0530

    git ai assist

---
+++ src/test/java/com/gitai/assist/AssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitai.assist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
